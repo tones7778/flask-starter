c3b5717ab9c314937a9a865bdcfd1138dfa024ca
tones7778 <tony.anania@gmail.com>
2018-09-23 21:26:34 -0400

    working

--- ssh-to-host.py
+++ ssh-to-host.py
@@ -6,13 +6,15 @@ import sqlite3
 import logging
 import configparser
 
-# read a list of hosts from file.
-# ssh to the linux host.
-# run and capture the results of 'uptime'
-# save to a sqlite db
 
 config = configparser.ConfigParser()
 config.read('secrets.ini')
+
+mydevices = config['auth']['hostname']  # catch bad hostname.
+mycommand = config['auth']['command']  # catch bad command. Can not seem to catch a bad command.
+myusername = config['auth']['username']  # catch bad username.
+mypassword = config['auth']['password']  # catch bad password or key
+
 
 logging.basicConfig(filename='demo.log',
                     level=logging.DEBUG,
@@ -20,16 +22,26 @@ logging.basicConfig(filename='demo.log',
 
 logging.info("------------------- START ---------------------------------------")
 
-mydevices = config['auth']['hostname']  # catch bad hostname.
-mycommand = 'cat /etc/redhat-release'  # catch bad command. Can not seem to catch a bad command.
-myusername = config['auth']['username']  # catch bad username.
-mypassword = config['auth']['password']  # catch bad password or key
 
 
 try:
+    print("Connection to host: ", mydevices)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(mydevices[0], username=myusername[0])
+    ssh.connect(mydevices, username=myusername, password=mypassword, timeout=30)
+
+except paramiko.AuthenticationException:
+    print("Authentication failed, please verify your credentials: %s")
+    quit()
+except paramiko.SSHException as sshException:
+    print("Unable to establish SSH connection: %s" % sshException)
+    quit()
+except paramiko.BadHostKeyException as badHostKeyException:
+    print("Unable to verify server's host key: %s" % badHostKeyException)
+    quit()
+except socket.error:
+    print(f"Could not connect to {i}")
+    quit()
 except Exception as error:
     print("ERROR: " + str(error))
     logging.debug("ERROR: " + str(error))
